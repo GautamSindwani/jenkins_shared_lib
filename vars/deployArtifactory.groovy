376cf6fccec6d9afa7689ecb27f7ee8d802c7005
GautamSindwani <76623186+GautamSindwani@users.noreply.github.com>
2023-11-26 20:50:18 +0530

    Update deployArtifactory.groovy

--- vars/deployArtifactory.groovy
+++ vars/deployArtifactory.groovy
@@ -1,5 +1,5 @@
 def call() {
-	def artifactoryUrl = 'http://54.210.121.167:8082/artifactory/example-repo-local/'
+	def artifactoryUrl = 'http://23.20.27.48:8082//artifactory/example-repo-local/'
 	def jarPath = "/home/ubuntu/Java_app_3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
 	def username='admin'
 	def password='Admin@123' // Replace with your Artifactory password
