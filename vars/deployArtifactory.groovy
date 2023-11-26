3c4a697acc3e849473490edf61f1d39a7644682d
GautamSindwani <76623186+GautamSindwani@users.noreply.github.com>
2023-11-27 00:26:03 +0530

    Update deployArtifactory.groovy

--- vars/deployArtifactory.groovy
+++ vars/deployArtifactory.groovy
@@ -1,22 +1,32 @@
+// def call() {
+// 	def artifactoryUrl = 'http://100.26.167.226:8082/artifactory/example-repo-local/'
+// 	// def jarPath = "/var/lib/jenkins/workspace/Jfrog_pipeline/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
+// 	def jarPath = "/home/ubuntu/Java_app_3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
+// 	def username='admin'
+// 	def password='Admin@123' // Replace with your Artifactory password
+
+// 	def curlCommand = """
+// 		curl -X PUT -u $username: $password -T $jarPath $artifactoryUrl
+// 	"""
+
+// 	def process = curlCommand.execute()
+// 	process.waitFor()
+
+
+// 	if (process.exitValue() == 0) {
+// 		println "Artifactory deployment successful!"
+// 		} else {
+// 			println "Artifactory deployment failed. Exit code: ${process.exitValue()}"
+// 			println "Error Output: ${process.err.text}"
+// 		}
+// 	}
 def call() {
-	def artifactoryUrl = 'http://100.26.167.226:8082/artifactory/example-repo-local/'
-	// def jarPath = "/var/lib/jenkins/workspace/Jfrog_pipeline/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
-	def jarPath = "/home/ubuntu/Java_app_3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
-	def username='admin'
-	def password='Admin@123' // Replace with your Artifactory password
-
-	def curlCommand = """
-		curl -X PUT -u $username: $password -T $jarPath $artifactoryUrl
-	"""
-
-	def process = curlCommand.execute()
-	process.waitFor()
-
-
-	if (process.exitValue() == 0) {
-		println "Artifactory deployment successful!"
-		} else {
-			println "Artifactory deployment failed. Exit code: ${process.exitValue()}"
-			println "Error Output: ${process.err.text}"
-		}
-	}
+    def artifactoryUrl = 'http://100.26.167.226:8082/artifactory/example-repo-local/'
+    def jarPath = "/var/lib/jenkins/workspace/Jfrog_pipeline/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
+    def username = 'admin'
+    def password = 'Admin@123' // Replace with your Artifactory password
+    catchError {
+	    sh "curl -X PUT -u $username:$password -T $jarPath $artifactoryUrl"
+    }
+    echo "Deployment finished."
+}
