47c95aaddeaac12a238bebbef24c3ef83cb04c5a
GautamSindwani <76623186+GautamSindwani@users.noreply.github.com>
2023-11-26 18:55:43 +0530

    Create deployArtifactory.groovy

---
+++ vars/deployArtifactory.groovy
@@ -0,0 +1,21 @@
+def call() {
+	def artifactoryUrl = 'http://54.210.121.167:8082/artifactory/example-repo-local/'
+	def jarPath = "/var/lib/jenkins/workspace/Demo_3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
+	def username='admin'
+	def password='Admin@123' // Replace with your Artifactory password
+
+	def curlCommand = """
+		curl -X PUT -u $username: $password -T $jarPath $artifactoryUrl
+	"""
+
+	def process = curlCommand.execute()
+	process.waitFor()
+
+
+	if (process.exitvalue()
+		println "Artifactory deployment successful!"
+		} else {
+			println "Artifactory deployment failed. Exit code: $(process.exitvalue()}"
+			println "Error Output: ${process.err.text}"
+		}
+	}
